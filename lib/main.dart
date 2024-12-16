b6af34a767b66aeab4c7ba852454899034e26e5e
NirajXtha <shresthaniraj633@gmail.com>
2024-12-16 19:26:19 +0545

    padding, textalign, decorations, boxshadows, text style

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,32 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("At the Top of the application"),
       ),
-      body: Container(
-        child: Text("At the body of the application"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          width: 100,
+          height: 100,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.purple,
+            borderRadius: BorderRadius.circular(10),
+            gradient: LinearGradient(colors: [Colors.pink, Colors.yellow]),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.blueGrey,
+                blurRadius: 10,
+              )
+            ]
+          ),
+          child: Text("This a box", 
+          textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              
+            ),),
+        ),
       ),
     );
   }
