5d3b57ef7e3ed9bd4d63706282051e14848c461d
NirajXtha <shresthaniraj633@gmail.com>
2024-12-18 17:08:52 +0545

    Rows and Columns

--- lib/main.dart
+++ lib/main.dart
@@ -23,31 +23,35 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("At the Top of the application"),
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8.0),
-          width: 100,
-          height: 100,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Colors.purple,
-            borderRadius: BorderRadius.circular(10),
-            gradient: LinearGradient(colors: [Colors.pink, Colors.yellow]),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.blueGrey,
-                blurRadius: 10,
-              )
-            ]
-          ),
-          child: Text("This a box", 
-          textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              
-            ),),
+      body: Container(
+        color: Colors.teal,
+        height: 200,
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              color: Colors.red,
+              width: 100,
+              height: 100,
+              alignment: Alignment.center,
+              ),
+              Container(
+              padding: const EdgeInsets.all(8.0),
+              color: Colors.purple,
+              width: 100,
+              height: 100,
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              color: Colors.blue,
+              width: 100,
+              height: 100,
+              alignment: Alignment.center,
+            ),
+          ],
         ),
       ),
     );
