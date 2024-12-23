cc9d6d20d54935e22bab2b52256621ca9a2c80be
NirajXtha <shresthaniraj633@gmail.com>
2024-12-23 17:39:26 +0545

    Cards, Stateless Widget, Assets

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,25 @@ import 'package:flutter/material.dart';
 void main(){
   // WidgetsApp // MaterialApp // CupertinoApp
   runApp(MaterialApp(
-    home: HomePage(),
+    home: const HomePage(),
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  var myText = "Change my text";
+  TextEditingController _nameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     // return Container(
@@ -20,14 +31,40 @@ class HomePage extends StatelessWidget {
     //   child: Text("Hi Flutter"),
     // );
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text("At the Top of the application"),
+        title: const Text("At the Top of the application"),
       ),
       body: Center(
-        child: Container(
-          color: Colors.teal,
-          height: 100,
-          width: 100,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                    "assets/bg.png",
+                    height: 500,
+                    // fit: BoxFit.cover,
+                  ),
+                  SizedBox(height: 20,),
+                  Text(myText, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+                  SizedBox(
+                  height: 20,
+                ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Enter your name",
+                        labelText: "Name",
+                    )),
+                  )
+                ]),
+            ),
+          ),
         ),
       ),
       drawer: Drawer(
@@ -39,7 +76,7 @@ class HomePage extends StatelessWidget {
               //   child: Text("Drawer Header"),
               //   decoration: BoxDecoration(color: Colors.blue),
               // ),
-              UserAccountsDrawerHeader(
+              const UserAccountsDrawerHeader(
                 accountName: Text("Niraj Shrestha"), 
                 accountEmail: Text("nishrestha776@gmal.com"),
                 // currentAccountPicture: Image.file()
@@ -48,19 +85,19 @@ class HomePage extends StatelessWidget {
                   backgroundImage: NetworkImage("https://iili.io/Jj884Eu.jpg"),),
               ),
               ListTile(
-                leading: Icon(Icons.home),
-                title: Text("Home"),
-                subtitle: Text("SubTitle"),
-                trailing: Icon(Icons.edit),
+                leading: const Icon(Icons.home),
+                title: const Text("Home"),
+                subtitle: const Text("SubTitle"),
+                trailing: const Icon(Icons.edit),
                 onTap: () {} ,
               ),
-              ListTile(
+              const ListTile(
               leading: Icon(Icons.person),
               title: Text("Home"),
               subtitle: Text("SubTitle"),
               trailing: Icon(Icons.edit),
             ),
-            ListTile(
+            const ListTile(
               leading: Icon(Icons.email),
               title: Text("Home"),
               subtitle: Text("SubTitle"),
@@ -70,8 +107,11 @@ class HomePage extends StatelessWidget {
           ),
         ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.edit),
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {});
+        },
+        child: const Icon(Icons.send),
         // mini: true, // to make the icon smaller
       ),
       // floatingActionButtonLocation: FloatingActionButtonLocation.startTop, // to move the button to the top or wherever u want
