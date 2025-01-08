588c8cda67b29cb92aa52c4c18a27d8b71aeb3b5
NirajXtha <shresthaniraj633@gmail.com>
2025-01-08 14:01:59 +0545

    http | json Decode | ListView & ListTile

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/screens/drawer.dart';
-import 'package:test_app/widgets/name_card_widget.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert'; // to decode json
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,8 +11,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var myText = "Change my text";
-  final TextEditingController _nameController = TextEditingController();
+  // var myText = "Change my text";
+  // final TextEditingController _nameController = TextEditingController();
+
+  var url = Uri.parse("https://dummyjson.com/products/search?q=phone&limit=10");
+  var data;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    var res = await http.get(url);
+    data = jsonDecode(res.body);
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,19 +45,24 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("At the Top of the application"),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            child:
-                NameCardWidget(myText: myText, nameController: _nameController),
-          ),
+      body: data != null 
+      ? ListView.builder(
+        itemBuilder: (context, index) { // context is the context of the widget | location in simple terms, index is the index of the item
+          return ListTile(
+            title: Text(data["products"][index]["title"]),
+            subtitle: Text("Description: ${data["products"][index]["description"]}"),
+            leading: Image.network(data["products"][index]["thumbnail"]),
+          );
+        },
+        itemCount: data["products"].length,
+      ) 
+      : const Center(
+          child: CircularProgressIndicator()
         ),
-      ),
       drawer: const MyDrawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          myText = _nameController.text;
+          // myText = _nameController.text;
           setState(() {});
         },
         child: const Icon(Icons.send),
